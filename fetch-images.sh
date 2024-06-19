12409936cca85fe9a2c80564085504cf9c59cbca
João Pedro Holanda <jpholanda.prf@gmail.com>
2024-06-19 20:50:03 -0300

    Batch requests in fetch-images.sh

--- fetch-images.sh
+++ fetch-images.sh
@@ -1,8 +1,32 @@
 #!/bin/sh
 
+# Maximum number of concurrent requests (batch size)
+max_concurrent_requests=50
+
+# Function to fetch a URL with curl
+fetch_image() {
+    mbid="$1"
+    echo "Fetching image for $mbid"
+    curl -sL https://coverartarchive.org/release/"$mbid"/front-250 --output dataset/images/"$mbid".jpg
+    echo "Fetched: $mbid.jpg"
+}
+
 mkdir -p dataset/images
 
+# Loop through URLs in batches
+index=0
 jq -r '.id' dataset/dataset.jsonl | while read -r mbid; do
-  echo "downloading image for $mbid"
-  curl -L https://coverartarchive.org/release/"$mbid"/front-250 --output dataset/images/"$mbid".jpg
+    index=$((index+1))
+
+    fetch_image "$mbid" &
+
+    if [ $((index % max_concurrent_requests)) -eq 0 ]
+    then
+      echo "Waiting previous $max_concurrent_requests to complete."
+      wait
+    fi
 done
+
+wait
+
+echo "All requests completed."
