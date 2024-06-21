499ee1e544d825b03a09bcd07f6595e257be9766
João Pedro Holanda <jpholanda.prf@gmail.com>
2024-06-21 19:20:01 -0300

    Read ids from stdin instead of from file

--- fetch-images.sh
+++ fetch-images.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 
 # Maximum number of concurrent requests (batch size)
-max_concurrent_requests=50
+max_concurrent_requests=200
 
 # Function to fetch a URL with curl
 fetch_image() {
     mbid="$1"
-    echo "Fetching image for $mbid"
     curl -sL https://coverartarchive.org/release/"$mbid"/front-250 --output dataset/images/"$mbid".jpg
-    echo "Fetched: $mbid.jpg"
+    echo $mbid >> dataset/processed_ids
 }
 
 mkdir -p dataset/images
 
 # Loop through URLs in batches
 index=0
-jq -r '.id' dataset/dataset.jsonl | while read -r mbid; do
+while read -r mbid; do
     index=$((index+1))
 
     fetch_image "$mbid" &
 
     if [ $((index % max_concurrent_requests)) -eq 0 ]
     then
-      echo "Waiting previous $max_concurrent_requests to complete."
       wait
+      echo "Downloaded $index images." 
     fi
 done
 
 wait
 
+echo "Downloaded $index images."
 echo "All requests completed."
