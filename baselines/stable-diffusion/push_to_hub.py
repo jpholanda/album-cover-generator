b0b46c4c278416c23dae94659cea768b19140c17
João Pedro Holanda <jpholanda.prf@gmail.com>
2024-06-23 12:08:34 -0300

    Delete stable diffusion baseline

--- baselines/stable-diffusion/push_to_hub.py
+++
@@ -1,5 +0,0 @@
-from diffusers import DiffusionPipeline
-
-if __name__ == '__main__':
-    pipe = DiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
-    pipe.push_to_hub("SD-cover-art")
